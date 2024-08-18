0a6548571d0a9da0575aaa0bbea9b55d8dad2d12
Janaviii <jingale28@gmail.com>
2024-08-18 10:37:20 +0530

    Committ

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { doc, getDoc, getDocs } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 import { addToCart } from '../../redux/cartSlice';
-import { fireDB } from '../../firebase/firebaseConfig';
+import { fireDB } from '../../firebase/FirebaseConfig';
 import Testimonial from '../../components/testimonial/Testimonial';
 
 function ProductInfo() {
